f29d51b59c971b59246c5173d003288cfe1d68c7
Taoge <wenter.wu@gmail.com>
2016-12-14 10:56:54 +0800

    >update

--- run-client.sh
+++ run-client.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -x 
+
+CLIENT_PROXY_PORT=$CLIENT_PROXY_PORT
+SERVER_PROXY_PORT=$SERVER_PROXY_PORT
+echo "proxy port: " $PROXY_PORT
 
 
+echo "server_addr: $NGROK_DOMAIN:$SERVER_PROXY_PORT" >>ngrok-config
 
-echo "server_addr: $NGROK_DOMAIN:4443" >>ngrok-config
 
-#the port 3000 is default ,you can change .
-
-exec ./bin/ngrok -config=ngrok-config -log=ngrok.log -proto="http" 3000
+exec ./bin/ngrok -config=ngrok-config -log=ngrok.log -proto="http" $PROXY_PORT
 
