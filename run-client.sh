8aaf7626fdd7ad8d7809d05f8845b38a9896ddfc
Taoge <wenter.wu@gmail.com>
2016-12-14 14:18:07 +0800

    > fix generate ngrok config

--- run-client.sh
+++ run-client.sh
@@ -3,7 +3,7 @@ set -x
 
 echo "client proxy port: " $CLIENT_PROXY_PORT
 
-echo "server_addr: $NGROK_DOMAIN:$SERVER_PROXY_PORT" >>ngrok-config
+echo "server_addr: $NGROK_DOMAIN:$SERVER_PROXY_PORT" >ngrok-config
 
 echo "you can find tunnel by cat ngrok.log "
 exec ./bin/ngrok -config=ngrok-config -log=ngrok.log -proto="http" $CLIENT_PROXY_PORT
