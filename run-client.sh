39b31cce5cb2b098b1788becc2aa287b4b8819be
Taoge <wenter.wu@gmail.com>
2016-12-14 11:17:25 +0800

    > fix proxy port typo

--- run-client.sh
+++ run-client.sh
@@ -3,11 +3,11 @@ set -x
 
 CLIENT_PROXY_PORT=$CLIENT_PROXY_PORT
 SERVER_PROXY_PORT=$SERVER_PROXY_PORT
-echo "proxy port: " $PROXY_PORT
+echo "client proxy port: " $CLIENT_PROXY_PORT
 
 
 echo "server_addr: $NGROK_DOMAIN:$SERVER_PROXY_PORT" >>ngrok-config
 
 
-exec ./bin/ngrok -config=ngrok-config -log=ngrok.log -proto="http" $PROXY_PORT
+exec ./bin/ngrok -config=ngrok-config -log=ngrok.log -proto="http" $CLIENT_PROXY_PORT
 
