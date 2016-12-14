d117b4fb46059e6f77d832e5612b900e52e3d849
Taoge <wenter.wu@gmail.com>
2016-12-14 12:12:26 +0800

    update

--- run-client.sh
+++ run-client.sh
@@ -5,5 +5,6 @@ echo "client proxy port: " $CLIENT_PROXY_PORT
 
 echo "server_addr: $NGROK_DOMAIN:$SERVER_PROXY_PORT" >>ngrok-config
 
+echo "you can find tunnel by cat ngrok.log "
 exec ./bin/ngrok -config=ngrok-config -log=ngrok.log -proto="http" $CLIENT_PROXY_PORT
 
