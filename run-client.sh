b5388fed2fecf658568efbb89ac8ab38fc55abaf
TaoGe <wenter.wu@gmail.com>
2017-07-05 19:13:03 +0800

    Update run-client.sh

--- run-client.sh
+++ run-client.sh
@@ -6,4 +6,4 @@ echo "client proxy port: " $CLIENT_PROXY_PORT
 echo "server_addr: $NGROK_DOMAIN:$SERVER_PROXY_PORT" >ngrok-config
 echo "inspect_addr: 0.0.0.0:4040" >>ngrok-config
 
-exec ./bin/ngrok -config=ngrok-config -log=stdout -proto="http" $CLIENT_PROXY_PORT
+exec ./bin/ngrok -config=ngrok-config -log=stdout -proto="http" -subdomain=$SUBDOMAIN $CLIENT_PROXY_PORT
