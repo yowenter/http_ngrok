2232720635e2b2e33f33b2dac97bd37ffe8bcc65
liuyan <yan.liu@@timanetworks.com>
2017-10-27 18:19:50 +0800

    打包

--- run-client.sh
+++ run-client.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-set -x 
-
+set -x
 echo "client proxy port: " $CLIENT_PROXY_PORT
-
-echo "server_addr: $NGROK_DOMAIN:$SERVER_PROXY_PORT" >ngrok-config
-echo "inspect_addr: 0.0.0.0:4040" >>ngrok-config
-
-exec ./bin/ngrok -config=ngrok-config -log=stdout -proto="http" -subdomain=$SUBDOMAIN $CLIENT_PROXY_PORT
+exec ./bin/ngrok -config=ngrok.cfg start http https ssh
